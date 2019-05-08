e71cdf9f92d8fd91b7407b9867995c6f6fbe978f
Shiela May Juele <ssjuele@up.edu.ph>
2019-05-08 21:48:19 +0800

    laravel project

---
+++ m/resources/views/home.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!DOCTYPE html>
+<html>
+<body>
+	<div class="main">
+		<div id="message">Pick up bottles</div>
+		<div class="bottles">
+			<div>Hi welcome newbie</div>
+			<div>Hello nice to meet you !</div>
+			<div>In this world full of mystery only faith brought us together</div>
+			<div>Have faith</div>
+			<div>your amazing no matter what </div>
+			<div>Have faith</div>
+			<div>your amazing no matter what </div>
+		</div>
+
+	</div>
+
+</body>
+</html>
+
+@endsection
