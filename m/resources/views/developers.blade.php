28fd966d092a4713a7647653517ae9de43dd916e
Shania Mariz Cruzado <sdcruzado@up.edu.ph>
2019-05-27 15:16:00 +0800

    about,faq,developers

---
+++ m/resources/views/developers.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>DEVELOPERS</title>
+	<link rel="stylesheet" href="styledev.css">
+</head>
+<style>
+	body{
+	background-color: #008B8B;
+}
+img{
+	display: block;
+	margin-left: auto;
+	margin-right: auto;
+	width: 100%;
+}
+p{
+  font-family: Courier;
+  margin-top: 30px;
+  font-size: 25px;
+  text-align: center;
+  letter-spacing: 3px;
+  color:  white;
+  margin-left: 50px;
+  margin-right: 50px;
+}
+</style>
+<body>
+
+	<img src="bl.jpg" class="responsive">
+	<img src="shiela.jpg" align="left" style ="width:30%;height=50%;margin-top: 26px; margin-bottom: 20px" class="responsive">
+	<img src="shania.jpg" align="right" style ="width:30%;height=50%;margin-top: 20px;margin-bottom: 20px" class="responsive">
+		<p>HELLO WORLD!<br>We are the developers of Bottela Letras. We are fourth year students taking up Bachelor of Science in Computer Science at the University of the Philippines Visayas. We made this application as a requirement in our CMSC 129 subject under Mr. Jenmarc Muchada.</p>
+</body>
+</html>
